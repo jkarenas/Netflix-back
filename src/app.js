e9ce6fa5655aba2ee4a6eab90cd2e15a429a9335
jkarenas <jc.arenas1993@gmail.com>
2023-12-04 13:01:45 -0500

    actualizacion

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ const express = require("express")
 const morgan = require("morgan")
 const bodyParser = require("body-parser")
 const route = require("./Routes/index")
-
+const cors = require( 'cors' );
 const app = express()
 
+app.use(cors())
 app.use(morgan("dev"))
 app.use(bodyParser.json())
 app.use("/", route)
